01c409dc260c49f410458fdbdcae4f03d01494fd
Kristof Galla <gallakristof@gmail.com>
2016-07-13 15:21:42 +0200

    Count projects as well in Company class

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import connect_to_db
 from company import Company
 
 # establish connection to database with 1. database name, 2. password
-cursor = connect_to_db.make_connection('levente', 'postgres')
+cursor = connect_to_db.make_connection('krs', '#3porcI.')
 
 # chose an option which picture to generate
 option = input('Press key 1, 2, 3 or 4 to select an option: ')
 
 if option == '1':
     # get the appropriate data from the database
-    cursor.execute("""SELECT DISTINCT company_name, string_agg(main_color, ', ') id
+    cursor.execute("""SELECT DISTINCT company_name, string_agg(main_color, ', '), count(id)
                       FROM project
                       GROUP BY company_name;""")
 
     # save each row of the extracted data as an instance in a list
     company_list = [Company(row) for row in cursor.fetchall()]
-    print(company_list[1].name, company_list[1].color)
+    print(company_list[1].name, company_list[1].color, company_list[1].project_num)
 
 else:
     print('You should choose option 1 for now, the others are not implemented yet')
--- sample-image-generator.py
+++ sample-image-generator.py
@@ -5,14 +5,14 @@ from PIL import ImageDraw
 img = Image.new("RGB", (512, 512), "red")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
-#font = ImageFont.truetype("sans-serif.ttf", 16)
+font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSerif.ttf", 26)
 text_options = {
     'fill': (255, 255, 255)
 }
 text_content = "Sample Text"
 text_size = draw.textsize(text_content)
 # draw.text((x, y),text_content,(r,g,b))
-draw.text((0, 0), text_content, **text_options)
+draw.text((0, 0), text_content, **text_options, font=font)
 draw.text((0, text_size[1]), text_content, **text_options)
 draw.text((text_size[0], 0), text_content, **text_options)
 draw.text(text_size, text_content, **text_options)
