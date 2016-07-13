8398b2f25ce6c7a4fdc012aca5b2285af1c65738
Kristof Galla <gallakristof@gmail.com>
2016-07-13 14:30:16 +0200

    Imlement average colour calculator function in Company class

--- company.py
+++ company.py
@@ -1,6 +1,26 @@
 class Company:
 
     def __init__(self, sql_output):
-        # sql_output should be a tuple
+        # sql_output should be a tuple of company name and color codes as one string
         self.name = sql_output[0]
-        self.colors = sql_output[1]
+        self.color = self.average_color(sql_output[1])
+
+
+    def average_color(self, all_colors):
+        # convert a string of colours to a list of colour strings (give default colour white if none)
+        if all_colors != None:
+            all_colors = all_colors.split(',')
+        else:
+            all_colors = ['#FFF']
+
+        r = 0
+        g = 0
+        b = 0
+
+        for i in all_colors:
+            hex_num = i.strip()
+            r += eval('0x' + hex_num[1] + hex_num[1])
+            g += eval('0x' + hex_num[2] + hex_num[2])
+            b += eval('0x' + hex_num[3] + hex_num[3])
+
+        return (round(r/len(all_colors)), round(g/len(all_colors)), round(b/len(all_colors)))
--- main.py
+++ main.py
@@ -9,13 +9,13 @@ option = input('Press key 1, 2, 3 or 4 to select an option: ')
 
 if option == '1':
     # get the appropriate data from the database
-    cursor.execute("""SELECT DISTINCT company_name, string_agg(main_color, ', ')
+    cursor.execute("""SELECT DISTINCT company_name, string_agg(main_color, ', ') id
                       FROM project
                       GROUP BY company_name;""")
 
     # save each row of the extracted data as an instance in a list
     company_list = [Company(row) for row in cursor.fetchall()]
-    print(company_list[0].name, company_list[0].colors)
+    print(company_list[1].name, company_list[1].color)
 
 else:
     print('You should choose option 1 for now, the others are not implemented yet')
