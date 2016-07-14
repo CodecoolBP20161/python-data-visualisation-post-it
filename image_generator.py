533b7d4c2701b65a0bf07a0054c7d5e920c8943e
Kristof Galla <gallakristof@gmail.com>
2016-07-14 11:41:53 +0200

    Make text be within the background boundaries

--- image_generator.py
+++ image_generator.py
@@ -3,7 +3,10 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import random
 
-img = Image.new("RGB", (640, 640), "black")
+bg_x = 1024 # x coordinate, number of columns
+bg_y = 640 # y coordinate, number of rows
+
+img = Image.new("RGB", (bg_x, bg_y), "black")
 draw = ImageDraw.Draw(img)
 
 
@@ -24,10 +27,11 @@ def generate_image(objects_to_print):
         text_content = objects_to_print[i].name
 
         # save the size of text in pixels
-        # text_size = draw.textsize(text_content)
+        text_size = draw.textsize(text_content, font=font)
 
         # draw.text((x, y),text_content,(r,g,b))
-        draw.text(get_place(), text_content, **text_options, font=font)
+        # text(position, text, fill=None, font=None)
+        draw.text(get_place(text_size), text_content, **text_options, font=font)
 
     # draw.text((0, text_size[1]), text_content, **text_options)
     # draw.text((text_size[0], 0), text_content, **text_options)
@@ -37,5 +41,5 @@ def generate_image(objects_to_print):
     img.show()
 
 
-def get_place():
-    return random.randint(0, 555), random.randint(0, 555)
+def get_place(text_size):
+    return random.randint(0, bg_x - text_size[0] ), random.randint(0, bg_y - text_size[1])
